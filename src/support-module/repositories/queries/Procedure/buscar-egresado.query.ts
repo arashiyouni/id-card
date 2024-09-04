f9a5fc668d7167e010e959223f443a29d9ff2e89
angie espinoza <angie.espinoza.campos@gmail.com>
2024-09-04 16:40:27 -0600

    rename file: Egresado

--- src/support-module/repositories/queries/Procedure/buscar-egresado.query.ts
+++ src/support-module/repositories/queries/Procedure/buscar-egresado.query.ts
@@ -4,19 +4,17 @@ import { DataSource} from "typeorm";
 
 
 @Injectable()
-export class Egresado {
+export class Procedure {
     constructor(
         @Inject('DATA_SOURCE_ACADEMICO')
         private readonly dataSource: DataSource
     ) { }
 
+    /** @description Procedimiento Almacenado que busca Egresado por carnet */
     async buscarEgresado(carnet: string) {
         const egresado = await this.dataSource
         .query("EXEC Egreso @0", [carnet])
 
-        return {
-            msg: 'Egresado Encontrado',
-            egresado
-        }
+        return  egresado
     }
 }
