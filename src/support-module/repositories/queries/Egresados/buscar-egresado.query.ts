dcd40cabf11b1b814f87a5d211c1a6d3e031c7d9
angie espinoza <angie.espinoza.campos@gmail.com>
2024-09-04 11:58:31 -0600

    rename query egresado and student

---
+++ src/support-module/repositories/queries/Egresados/buscar-egresado.query.ts
@@ -0,0 +1,23 @@
+import { Inject, Injectable } from "@nestjs/common";
+import { DataSource} from "typeorm";
+
+
+
+@Injectable()
+export class Egresado {
+    constructor(
+        @Inject('DATA_SOURCE_ACADEMICO')
+        private readonly dataSource: DataSource
+    ) { }
+
+    /** @description Procedimiento Almacenado que busca Egresado por carnet */
+    async buscarEgresado(carnet: string) {
+        const egresado = await this.dataSource
+        .query("EXEC Egreso @0", [carnet])
+
+        return {
+            msg: 'Egresado Encontrado',
+            egresado
+        }
+    }
+}
