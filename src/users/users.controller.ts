207792378aeb5388fa1b3c605ef27b505614593e
angie espinoza <angie.espinoza.campos@gmail.com>
2024-09-10 17:54:44 -0600

    endpoint to load photo

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,7 @@
-import { Body, Controller, Delete, Get, HttpCode, InternalServerErrorException, NotFoundException, Param, Post, Query, UseGuards, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, InternalServerErrorException, NotFoundException, Param, Post, Query, UploadedFile, UseGuards, UseInterceptors, ValidationPipe } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CarnetDTO } from './dto/carnet.dto';
-import { FotoDTO } from './dto/foto-carnet.dto';
+import {  StudentDTO } from './dto/foto-carnet.dto';
 //import { RolesGuard } from 'src/auth/roles.guard';
 // import { Role } from 'src/common/interface/role.enum';
 // import { Roles } from 'src/common/decorator/decorator.decorator';
@@ -35,7 +35,7 @@ export class UsersController {
 
     @Post('foto')
     @HttpCode(200)
-    async enviarFotoCarnet(@Body() foto: FotoDTO){
-        return await this.userService.enviarFotoCarnet(foto)
+    async guardarFotoCarnet(@Body() student: StudentDTO){
+        return await this.userService.fotoCarnet(student)
     }
 }
