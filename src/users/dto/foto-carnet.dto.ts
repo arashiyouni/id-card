1c92a74ef9a718ee4bac746ba14ec29153d55436
angie espinoza <angie.espinoza.campos@gmail.com>
2024-09-10 17:45:16 -0600

    refactor dto foto-carnet

--- src/users/dto/foto-carnet.dto.ts
+++ src/users/dto/foto-carnet.dto.ts
@@ -3,36 +3,18 @@ import { IsBase64, IsDate, IsEmail, IsNumber, IsOptional, IsString } from "class
 //LA IMAGEN SE GUARDA EN BINARIO SI ES SQL Y MONGO EN BASE64
 export class StudentDTO {
 
-    @IsNumber()
-    activo: number
-
-    @IsString()
-    apellidos: string
-
     @IsString()
     carnet: string
 
     @IsEmail()
     email: string
 
-    // @IsBase64()
+    @IsBase64()
     @IsString()
     Foto: string
 
     @IsString()
     TipoCarnet: string
-
-    @IsString()
-    Nombres: string
-
-    // @IsString()
-    // idfacultad: string
-
-    @IsString()
-    NombreFacultad: string
-
-    @IsString()
-    NombreCarrera: string
 
     @IsString()
     CicloCarnetizacion: string
