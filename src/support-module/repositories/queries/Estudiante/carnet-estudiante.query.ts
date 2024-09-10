2fe083575c9bce072c8c59f8a0f9704f0b1ca472
angie espinoza <angie.espinoza.campos@gmail.com>
2024-09-10 17:53:15 -0600

    repositories inser photo student and search carnet equivalente

--- src/support-module/repositories/queries/Estudiante/carnet-estudiante.query.ts
+++ src/support-module/repositories/queries/Estudiante/carnet-estudiante.query.ts
@@ -1,20 +1,24 @@
 import { Inject } from "@nestjs/common";
+import { CarneEquivalente } from "src/models/UFGRegistroAcademico-Entities/CarneEquivalente";
 import { Repository } from "typeorm";
 
 
 export class CarnetEstudiante {
     constructor(
         @Inject('CARNET_EQUIVALENTE_REPOSITORY')
-        private carnetEquivalenteRepository: Repository<CarnetEstudiante>
+        private carnetEquivalenteRepository: Repository<CarneEquivalente>
     ){}
 
     async buscarCarnetEquivalente(carnet: string){
         const carnetEquivalente = await this.carnetEquivalenteRepository
         .createQueryBuilder('carnet')
         .select(['carnet.carnetEquivalente'])
-        .where('carne = :idCarnet', {idCarnet: carnet})
+        .where('carnet = :idCarnet', {idCarnet: carnet})
         .getOne()
 
-        return carnetEquivalente
+
+        if(!carnetEquivalente) return ''
+
+        return carnetEquivalente.carnetEquivalente
     }
 }
--- src/support-module/repositories/queries/Estudiante/foto-estudiante.query.ts
+++ src/support-module/repositories/queries/Estudiante/foto-estudiante.query.ts
@@ -1,4 +1,5 @@
 import { Inject, Injectable } from "@nestjs/common";
+import { FotoHexa } from "src/common/interface/sql/parameters/insertar-foto";
 import { Pictures } from "src/models/RegAcademico-Entities/Pictures.entity";
 import { Repository } from "typeorm";
 
@@ -8,6 +9,19 @@ import { Repository } from "typeorm";
 export class FotoEstudiante {
     constructor(
         @Inject('PICTURES_REPOSITORY')
-        private fotoEstudiante: Repository<Pictures>
+        private fotoEstudianteRepository: Repository<Pictures>
     ){}
+
+    async insertarFotoSql(data: FotoHexa){
+        const fotoSql = this.fotoEstudianteRepository
+            .createQueryBuilder()
+            .insert()
+            .into(Pictures)
+            .values([
+                {id: data.carnet, length: data.length, picture: data.foto, type: data.idSede, fecha: data.date}
+            ])
+            .execute()
+
+        return fotoSql
+    }
 }
